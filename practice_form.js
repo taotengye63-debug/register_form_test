a42db79d97db0c409ebed22fd2d77af303c62bfc
taotengye63-debug <taotengye63@gmail.com>
2026-06-17 23:04:31 +0900

    The second day's result

---
+++ practice_form.js
@@ -0,0 +1,18 @@
+console.log("JS読み込み成功")
+
+const iframe = document.getElementById("hidden_iframe2");
+
+let submitted = false;
+
+document
+    .getElementById("reservation_form2")
+    .addEventListener("submit", function () {
+        submitted = true;
+    });
+
+iframe.onload = function () {
+    if (submitted) {
+        document.body.innerHTML =
+            `<div><h1>ご予約を受け付けました。</h1></div>`;
+    }
+};
